60836dd9808c537ccdcba72287c2fcde279ef6d8
KrippleDD <kirill.drik.717@gmail.com>
2022-05-25 19:39:19 +0300

    addhomework3

---
+++ homework3.py
@@ -0,0 +1,18 @@
+#ДЗ №3 Парты и школьники
+
+
+
+kidsinclass1 = input(20)
+kidsinclass2 = input(26)
+kidsinclass3 = input(30)
+
+
+
+
+answer1 = kidsinclass1 // 2
+answer2 = kidsinclass2 // 2
+answer3 = kidsinclass3 // 2
+
+print('Ответ', answer1)
+print('Ответ', answer2)
+print('Ответ', answer3)
